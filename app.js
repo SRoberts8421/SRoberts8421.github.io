3f45175f0e63dac7e7599f7be5e8f5878ae87a14
SRoberts8421 <71564092+SRoberts8421@users.noreply.github.com>
2023-11-19 17:13:48 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 async function getData(selected_meal) {
-    var response = await fetch('menu_items.json');   // this is a GET request
+    var response = await fetch('cit5students.json');   // this is a GET request
 
     if(response.ok) {
         var data = await response.json();
 
         // filter data array for the selected meal
-        meal_items = data.filter( (item) => item.meal == selected_meal );  
+        meal_items = data.filter( (item) => item.major == selected_meal );  
 
        var templateText = document.getElementById('menuTemplate').innerHTML;
        var compiledTemplateText = Handlebars.compile(templateText);   // get and compile template code
